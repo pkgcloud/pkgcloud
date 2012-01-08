0cdd5090152ad2f08730104618274c058f6978b9
indexzero <charlie.robbins@gmail.com>
2012-01-08 07:05:07 -0500

    [api test] Update Rackspace cloudservers integration to ensure all tests except test/rackspace/compute/servers-test.js all pass

---
+++ test/helpers/index.js
@@ -0,0 +1,22 @@
+var fs = require('fs'),
+    path = require('path'),
+    pkgcloud = require('../../lib/pkgcloud');
+
+var helpers = exports;
+
+helpers.createClient = function createClient(provider, service, config) {
+  config = config || helpers.loadConfig(provider);
+  config.provider = provider;
+
+  return pkgcloud[service].createClient(config);
+};
+
+helpers.loadConfig = function loadConfig(provider) {
+  var content = fs.readFileSync(path.join(
+      __dirname,
+      '../configs/',
+      provider + '.json'
+  ), 'utf8');
+
+  return JSON.parse(content);
+};
