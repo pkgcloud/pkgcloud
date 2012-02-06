c10b289fe0b37b9f882241cc2a02fe80b0917f2e
Nuno Job <nunojobpinto@gmail.com>
2012-02-06 11:32:36 +0000

    [joyent] get flavors and tests seem to be complete

--- test/joyent/compute/flavor-test.js
+++ test/joyent/compute/flavor-test.js
@@ -20,7 +20,6 @@ vows.describe('pkgcloud/joyent/compute/flavors').addBatch({
     "the getFlavors() method": {
       "with no details": {
         topic: function () {
-          console.log(client)
           client.getFlavors(this.callback);
         },
         "should return the list of flavors": function (err, flavors) {
@@ -32,18 +31,27 @@ vows.describe('pkgcloud/joyent/compute/flavors').addBatch({
       },
       "with details": {
         topic: function () {
-          client.getFlavors(true, this.callback);
+          client.getFlavors(client.config.username, this.callback);
         },
         "should return the list of flavors": function (err, flavors) {
           flavors.forEach(function (flavor) {
             assert.assertFlavorDetails(flavor);
           });
         }
+      },
+      "but should fail when requesting an account we do not own": {
+        topic: function () {
+          client.getFlavors('joyent', this.callback);
+        },
+        "returns an unauthorized access error": function (err, flavors) {
+          assert.ok(err);
+          assert.ok(err.httpCode === 403);
+        }
       }
     }
   }
 }).addBatch({
-  "The pkgcloud Rackspace compute client": {
+  "The pkgcloud Joyent compute client": {
     "the getFlavor() method": {
       topic: function () {
         client.getFlavor(testContext.flavors[0].id, this.callback);
@@ -53,4 +61,4 @@ vows.describe('pkgcloud/joyent/compute/flavors').addBatch({
       }
     }
   }
-}).export(module);
+})["export"](module);
