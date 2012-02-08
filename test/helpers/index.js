234429ca75731fd761e59624e31df47d3d516a84
Nuno Job <nunojobpinto@gmail.com>
2012-02-08 14:35:03 +0000

    [joyent] refactoring to use request instead of smartdc
    added http signing to the mix

--- test/helpers/index.js
+++ test/helpers/index.js
@@ -7,6 +7,37 @@ var helpers = exports;
 helpers.createClient = function createClient(provider, service, config) {
   config = config || helpers.loadConfig(provider);
   config.provider = provider;
+
+  // use your key for testing, so our credentials dont need to go in the repo
+  if(provider === 'joyent') {
+    if(!config.username) {
+      if (!config.account)
+        config.account = process.env.SDC_CLI_ACCOUNT;
+
+      if (!config.identity) {
+        if (process.env.SDC_CLI_IDENTITY) {
+          config.identity = process.env.SDC_CLI_IDENTITY;
+        } else {
+          config.identity = process.env.HOME + '/.ssh/id_rsa';
+        }
+      }
+
+      if (!config.keyId) {
+        if (process.env.SDC_CLI_KEY_ID) {
+          config.keyId = process.env.SDC_CLI_KEY_ID;
+        } else {
+          config.keyId = 'id_rsa';
+        }
+      }
+      
+      if(config.account) {
+        config.keyId = '/' + config.account + '/keys/' + config.keyId;
+        config.key   = fs.readFileSync(config.identity,'ascii');
+      } else {
+        throw new Error("Can't test without username and account");
+      }
+    }
+  }
   return pkgcloud[service].createClient(config);
 };
 
