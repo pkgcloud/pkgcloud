627ebb6dde3d9b82ca9fa71921ae1e9fb7b38eae
cronopio <aristizabal.daniel@gmail.com>
2012-02-11 15:49:16 -0500

    [flavors] Initial test for flavors methods

---
+++ test/rackspace/databases/flavor-test.js
@@ -0,0 +1,27 @@
+/*
+ * flavor-test.js: Test for pkgcloud Rackspace database flavor requests
+ * 
+ * (C) 2010 Nodejitsu Inc.
+ * 
+ */
+
+var vows = require('vows'),
+    assert = require('../../helpers/assert'),
+    helpers = require('../../helpers');
+
+var client = helpers.createClient('rackspace', 'database');
+
+vows.describe('pkgcloud/rackspace/database/flavors').addBatch({
+  "The pkgcloud Rackspace database client": {
+    "the getFlavors() method": {
+      topic: function () {
+        client.getFlavors(this.callback);
+      },
+      "should return the list of flavors": function (err, flavors) {
+        assert.isNull(err);
+        console.log(flavors);
+        assert.isTrue(false);
+      }
+    }
+  }
+}).export(module);
