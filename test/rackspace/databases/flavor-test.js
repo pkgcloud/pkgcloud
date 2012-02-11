6aa39b26be2798d3ef379af99434634e1361bf0c
cronopio <aristizabal.daniel@gmail.com>
2012-02-11 16:17:07 -0500

    [flavors] added test for flavors with details

--- test/rackspace/databases/flavor-test.js
+++ test/rackspace/databases/flavor-test.js
@@ -14,13 +14,25 @@ var client = helpers.createClient('rackspace', 'database');
 vows.describe('pkgcloud/rackspace/database/flavors').addBatch({
   "The pkgcloud Rackspace database client": {
     "the getFlavors() method": {
-      topic: function () {
-        client.getFlavors(this.callback);
+      "with now details": {
+        topic: function () {
+          client.getFlavors(this.callback);
+        },
+        "should return the list of flavors": function (err, flavors) {
+          assert.isNull(err);
+          console.log(flavors);
+          assert.isTrue(false);
+        }
       },
-      "should return the list of flavors": function (err, flavors) {
-        assert.isNull(err);
-        console.log(flavors);
-        assert.isTrue(false);
+      "with details": {
+        topic: function () {
+          client.getFlavors(true, this.callback);
+        },
+        "should return the list of flavors with details": function (err, flavors) {
+          assert.isNull(err);
+          console.log(flavors);
+          assert.isTrue(false);
+        }
       }
     }
   }
