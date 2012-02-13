f64a964ba1f2d8524b16466c7fc7f994ef399fca
cronopio <aristizabal.daniel@gmail.com>
2012-02-13 14:02:02 -0500

    [tests] copying macros and doing compatible with v1.1 of auth api of rackspace

--- test/rackspace/databases/authentication-test.js
+++ test/rackspace/databases/authentication-test.js
@@ -14,12 +14,57 @@ var vows = require('vows'),
 var testData = {},
     client = helpers.createClient('rackspace', 'database');
 
+function shouldAuthNew (client) {
+  return {
+    topic: function () {
+      client.auth(this.callback);
+    },
+    "should respond with 200 and appropriate info": function (err, res) {
+      assert.equal(res.statusCode, 200);
+      assert.isObject(res.headers);
+      assert.isObject(res.body);
+    },
+    "should respond with a token": function (err, res) {
+      assert.isObject(res.body.auth);
+      assert.isObject(res.body.auth.token);
+      assert.equal(typeof(res.body.auth.token.id), 'string');
+    },
+    "should update the config with appropriate urls": function (err, res) {
+      var config = client.config;
+      assert.equal(res.headers['x-server-management-url'], config.serverUrl);
+      assert.equal(res.headers['x-storage-url'], config.storageUrl);
+      assert.equal(res.headers['x-cdn-management-url'], config.cdnUrl);
+      assert.equal(res.headers['x-auth-token'], config.authToken);
+    }
+  };
+}
+
+function shouldNotAuthNew (service) {
+  return {
+    topic: function () {
+      var badClient = helpers.createClient('rackspace', service, {
+        "auth": {
+          "username": "fake",
+          "apiKey": "data"
+        }
+      });
+
+      badClient.auth(this.callback);
+    },
+    "should respond with Error code 401": function (err, res) {
+      assert.isObject(err);
+      assert.isObject(err.unauthorized);
+      assert.equal(err.unauthorized.code, 401);
+    }
+  }
+};
+
+
 vows.describe('pkgcloud/rackspace/database/authentication').addBatch({
   "The pkgcloud Rackspace database client": {
     "should have core methods defined": macros.shouldHaveCreds(client),
     "the getVersion() method": {
       topic: function () {
-        console.log(client.getVersion);
         client.getVersion(this.callback);
       },
       "should return the proper version": function (versions) {
@@ -28,8 +73,8 @@ vows.describe('pkgcloud/rackspace/database/authentication').addBatch({
       }
     },
     "the auth() method": {
-      "with a valid username and api key": macros.shouldAuthenticate(client),
-      "with an invalid username and api key": macros.shouldNotAuthenticate('database')
+      "with a valid username and api key": shouldAuthNew(client),
+      "with an invalid username and api key": shouldNotAuthNew('database')
     }
   }
 }).export(module);
