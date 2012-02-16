20c117a20643df606848f930880068817f24e522
Nuno Job <nunojobpinto@gmail.com>
2012-02-16 18:32:49 +0000

    [storage] removed tests that were just stubs

--- test/rackspace/storage/storage-object-metadata-test.js
+++ test/rackspace/storage/storage-object-metadata-test.js
@@ -5,84 +5,84 @@
  * MIT LICENSE
  *
  */
-
-var path = require('path'),
-    vows = require('vows'),
-    fs = require('fs'),
-    assert = require('assert'),
-    pkgcloud = require('../../../lib/pkgcloud'),
-    helpers = require('../../helpers');
-
-var client = helpers.createClient('rackspace', 'storage'),
-    fixturesDir = path.join(__dirname, '..', '..', 'fixtures'),
-    sampleData = fs.readFileSync(path.join(fixturesDir, 'fillerama.txt'), 'utf8'),
-    testData = {};
-
-vows.describe('pkgcloud/rackspace/storage/file/metadata').addBatch({
-  "The pkgcloud Rackspace storage client": {
-    "an instance of a Container object": {
-      "the upload() method": {
-        topic: function () {
-          var ustream = client.addFile('test_container', {
-            remote: 'file1.txt',
-            local: path.join(__dirname, '..', 'test', 'fixtures', 'fillerama.txt')
-          }, function () { });
-
-          ustream.on('end', this.callback)
-        },
-        "should raise the `end` event": function () {
-          assert.isTrue(true);
-        }
-      }
-    }
-  }
-}).addBatch({
-  "The pkgcloud Rackspace storage client": {
-    "the getFile() method": {
-      "for a file that exists": {
-        topic: function () {
-          client.getFile('test_container', 'file1.txt', this.callback);
-        },
-        "should return a valid StorageObject": function (err, file) {
-          helpers.assertFile(file);
-          testData.file = file;
-        }
-      }
-    }
-  }
-})/*.addBatch({
-  "The pkgcloud Rackspace storage client": {
-    "the addMetadata() method": {
-      topic: function () {
-        testData.file.addMetadata({ "ninja": 'true' }, this.callback);
-      },
-      "should response with true": function (err, added) {
-        assert.isTrue(added);
-      }
-    }
-  }
-}).addBatch({
-  "The pkgcloud Rackspace storage client": {
-    "the getMetadata() method": {
-      topic: function () {
-        testData.file.getMetadata(this.callback);
-      },
-      "should response with true": function (err, added) {
-        assert.isTrue(added);
-      }
-    }
-  }
-})*/.addBatch({
-  "The pkgcloud Rackspace storage client": {
-    "the destroyFile() method": {
-      "for a file that exists": {
-        topic: function () {
-          client.destroyFile('test_container', 'file1.txt', this.callback);
-        },
-        "should return true": function (err, deleted) {
-          assert.isTrue(deleted);
-        }
-      }
-    }
-  }
-}).export(module);
+//
+//var path = require('path'),
+//    vows = require('vows'),
+//    fs = require('fs'),
+//    assert = require('assert'),
+//    pkgcloud = require('../../../lib/pkgcloud'),
+//    helpers = require('../../helpers');
+//
+//var client = helpers.createClient('rackspace', 'storage'),
+//    fixturesDir = path.join(__dirname, '..', '..', 'fixtures'),
+//    sampleData = fs.readFileSync(path.join(fixturesDir, 'fillerama.txt'), 'utf8'),
+//    testData = {};
+//
+//vows.describe('pkgcloud/rackspace/storage/file/metadata').addBatch({
+//  "The pkgcloud Rackspace storage client": {
+//    "an instance of a Container object": {
+//      "the upload() method": {
+//        topic: function () {
+//          var ustream = client.addFile('test_container', {
+//            remote: 'file1.txt',
+//            local: path.join(__dirname, '..', 'test', 'fixtures', 'fillerama.txt')
+//          }, function () { });
+//
+//          ustream.on('end', this.callback)
+//        },
+//        "should raise the `end` event": function () {
+//          assert.isTrue(true);
+//        }
+//      }
+//    }
+//  }
+//}).addBatch({
+//  "The pkgcloud Rackspace storage client": {
+//    "the getFile() method": {
+//      "for a file that exists": {
+//        topic: function () {
+//          client.getFile('test_container', 'file1.txt', this.callback);
+//        },
+//        "should return a valid StorageObject": function (err, file) {
+//          helpers.assertFile(file);
+//          testData.file = file;
+//        }
+//      }
+//    }
+//  }
+//})/*.addBatch({
+//  "The pkgcloud Rackspace storage client": {
+//    "the addMetadata() method": {
+//      topic: function () {
+//        testData.file.addMetadata({ "ninja": 'true' }, this.callback);
+//      },
+//      "should response with true": function (err, added) {
+//        assert.isTrue(added);
+//      }
+//    }
+//  }
+//}).addBatch({
+//  "The pkgcloud Rackspace storage client": {
+//    "the getMetadata() method": {
+//      topic: function () {
+//        testData.file.getMetadata(this.callback);
+//      },
+//      "should response with true": function (err, added) {
+//        assert.isTrue(added);
+//      }
+//    }
+//  }
+//})*/.addBatch({
+//  "The pkgcloud Rackspace storage client": {
+//    "the destroyFile() method": {
+//      "for a file that exists": {
+//        topic: function () {
+//          client.destroyFile('test_container', 'file1.txt', this.callback);
+//        },
+//        "should return true": function (err, deleted) {
+//          assert.isTrue(deleted);
+//        }
+//      }
+//    }
+//  }
+//}).export(module);
