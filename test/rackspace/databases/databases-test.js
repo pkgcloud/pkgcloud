7ae4178e8b037a4e5c20340ba732a9673f628b45
cronopio <aristizabal.daniel@gmail.com>
2012-03-04 13:06:33 -0500

    [databases] pass working for create, destroy and get methods of databases within instances

--- test/rackspace/databases/databases-test.js
+++ test/rackspace/databases/databases-test.js
@@ -61,23 +61,50 @@ vows.describe('pkgcloud/rackspace/databases/databases').addBatch({
   }
 }).addBatch({
   "The pkgcloud Rackspace Database client": {
-    "the create() method": {
+    "the getDatabases() method": {
       topic: function () {
-        this.callback();
+        var self = this;
+        client.getInstances(function (err, instances) {
+          var ready = instances.filter(function (instance) {
+            return (instance.status == 'ACTIVE');
+          });
+          if (ready.length === 0) {
+            console.log('ERROR:   Is necessary have Instances actived for test the create of database');
+          }
+          client.getDatabases(ready[0], self.callback);
+        });
       },
-      "should return a recent database": function (err, database) {
-        assert.isTrue(false);
+      "should return a list of databases": function (err, list, response) {
+        assert.isNull(err);
+        assert.isArray(list);
+        assert.ok(list.length > 0);
+        assert.equal(response.statusCode, 200);
+      },
+      "the list should have names": function (err, list, response) {
+        assert.ok(list[0]);
+        assert.ok(list[0].name);
+        assert.isString(list[0].name);
       }
     }
   }
 }).addBatch({
   "The pkgcloud Rackspace Database client": {
-    "the delete() method": {
+    "the destroyDatabase() method": {
       topic: function () {
-        this.callback();
+        var self = this;
+        client.getInstances(function (err, instances) {
+          var ready = instances.filter(function (instance) {
+            return (instance.status == 'ACTIVE');
+          });
+          if (ready.length === 0) {
+            console.log('ERROR:   Is necessary have Instances actived for test the create of database');
+          }
+          client.destroyDatabase('TestDatabase', ready[0], self.callback);
+        });
       },
-      "should delete the database": function (err) {
-        assert.isTrue(false);
+      "should respond correctly": function (err, response) {
+        assert.isNull(err);
+        assert.equal(response.statusCode, 202);
       }
     }
   }
