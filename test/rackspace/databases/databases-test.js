4f39ecc9bce6f5d6101eee0fcdbf71f36b98c69c
cronopio <aristizabal.daniel@gmail.com>
2012-03-04 12:14:30 -0500

    [databases] test pass for create database within a instance

--- test/rackspace/databases/databases-test.js
+++ test/rackspace/databases/databases-test.js
@@ -14,12 +14,48 @@ var client = helpers.createClient('rackspace', 'database');
 
 vows.describe('pkgcloud/rackspace/databases/databases').addBatch({
   "The pkgcloud Rackspace Database client": {
-    "the getDatabases() method": {
+    "the createDatabases() method": {
       topic: function () {
-        this.callback();
+        var self = this;
+        client.getInstances(function (err, instances) {
+          var ready = instances.filter(function (instance) {
+            return (instance.status == 'ACTIVE');
+          });
+          if (ready.length === 0) {
+            console.log('ERROR:   Is necessary have Instances actived for test the create of database');
+          }
+          client.createDatabase({name: 'TestDatabase', instance:ready[0]}, self.callback);
+        });
       },
-      "should return list of databases by instance": function (err, list) {
-        assert.isTrue(false);
+      "should respond correctly": function (err, response) {
+        assert.isNull(err);
+        assert.equal(response.statusCode, 202);
+      }
+    }
+  }
+}).addBatch({
+  "The pkgcloud Rackspace Database client": {
+    "the createDatabase() method with no name": {
+      topic: function () {
+        client.createDatabase({}, this.callback);
+      },
+      "should get error for name": function (err, response) {
+        assert.isObject(err);
+        assert.isString(err.message);
+        assert.isUndefined(response);
+      }
+    }
+  }
+}).addBatch({
+  "The pkgcloud Rackspace Database client": {
+    "the createDatabase() method with no instance": {
+      topic: function () {
+        client.createDatabase({name:'NotCreated'}, this.callback);
+      },
+      "should get error for instance": function (err, response) {
+        assert.isObject(err);
+        assert.isString(err.message);
+        assert.isUndefined(response);
       }
     }
   }
