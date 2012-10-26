2f134d956bb3fa2013478959befb3a93606aafdd
Dale Stammen <stammen@gmail.com>
2012-10-26 08:37:47 -0700

    start destroyServer. start azureServer-test.js

--- test/azure/azureServer-test.js
+++ test/azure/azureServer-test.js
@@ -2,13 +2,11 @@
 
 var azureServer = require('../../lib/pkgcloud/azure/compute/client/azure/azureServer.js');
 var fs = require('fs');
-var async = require('async');
 
 var options = {
-  name: "create-test-ids1",
-  flavor: "Small",
-  image: "create-test-ids2 "
-  //image: "MSFT__Windows-Server-2012-Datacenter-201208.01-en.us-30GB.vhd"
+  name: "create-test-ids2",
+  flavor: "ExtraSmall",
+  image: "CANONICAL__Canonical-Ubuntu-12-04-amd64-server-20120528.1.3-en-us-30GB.vhd"
 };
 
 // load azure config file
@@ -17,43 +15,15 @@ var config = JSON.parse(fs.readFileSync("../configs/azure.json",'utf8'));
 
 var testServer = new azureServer.AzureServerInfo('create-test-ids2','create-test-ids2');
 
-var az = new azureServer.AzureServer(config);
+var server = new azureServer.AzureServer(config);
+server.destroyServer(testServer, function(err, results) {
 
-var createServer = true;
-var destroyServer = true;
-
-async.waterfall([
-  function(callback) {
-    if(!createServer) {
-      callback(null, testServer);
-      return;
-    }
-    console.log("creating server:" + options.name)
-    az.createServer(options, function(err, server) {
-       if(err) {
-         callback(err);
-       } else {
-         callback(null, server);
-       }
-     });
-  }/*,
-  function(server, callback) {
-    console.log("destroying server:" + server.name)
-    az.destroyServer(server, function(err, result) {
-      callback(err, result);
-    });
-  }*/],
-  function (err, result) {
-    if(err) {
-      console.dir(err);
-    } else {
-      console.dir('ok');
-    }
+  if(err) {
+    console.log(err);
+  } else {
+    console.dir(results);
   }
-);
-
-
-
+});
 
 /*
 
