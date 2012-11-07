957701ae715c298389919cb46d81bb528d576581
Dale Stammen <stammen@gmail.com>
2012-11-07 12:42:05 -0800

    stopServer and pollRequestStatus rest api

---
+++ test/azure/compute/client/test-destroyServer.js
@@ -0,0 +1,20 @@
+//TODO: Make this a vows test
+
+var Client = new require('../../../../lib/pkgcloud/core/base/client').Client;
+var helpers = require('../../../helpers');
+
+var client = helpers.createClient('azure2', 'compute');
+
+var options = {};
+
+client.destroyServer('pkgcloud2', function(err, result) {
+  if(err) {
+    console.log(err);
+  } else {
+    console.dir(result);
+  }
+});
+
+
+
+
