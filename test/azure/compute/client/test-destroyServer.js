314d434f8a9cfdaa02f4b4bfac7a9d728f62e06a
Dale Stammen <stammen@gmail.com>
2012-11-07 16:16:52 -0800

    destroyServer now removes OSDisk and blob storage

--- test/azure/compute/client/test-destroyServer.js
+++ test/azure/compute/client/test-destroyServer.js
@@ -2,12 +2,15 @@
 
 var Client = new require('../../../../lib/pkgcloud/core/base/client').Client;
 var helpers = require('../../../helpers');
+var pkgcloud = require('../../../../lib/pkgcloud');
 
+//pkgcloud.storage.createClient()
 var client = helpers.createClient('azure2', 'compute');
 
 var options = {};
 
-client.destroyServer('pkgcloud1', function(err, result) {
+
+client.destroyServer('pkgcloud11', function(err, result) {
   if(err) {
     console.log(err);
   } else {
