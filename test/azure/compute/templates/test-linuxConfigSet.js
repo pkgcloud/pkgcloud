215ab90f000610f162cdff011022ef61f1c81daa
Dale Stammen <stammen@gmail.com>
2012-11-08 09:56:34 -0800

    start of createServer rest api

---
+++ test/azure/compute/templates/test-linuxConfigSet.js
@@ -0,0 +1,24 @@
+//TODO: Make this a vows test
+
+var Client = new require('../../../../lib/pkgcloud/core/base/client').Client;
+var helpers = require('../../../helpers');
+var templates = require('../../../../lib/pkgcloud/azure2/compute/templates/templates');
+var _ = require('underscore');
+
+var fs = require('fs');
+
+var params = {
+  HOSTNAME: 'pkgcloud1',
+  USERNAME: 'stammen',
+  PASSWORD: 'pkgcloud!!',
+  SSH_CERTIFICATE_FINGERPRINT: '123456789'
+};
+
+templates.load('linuxConfigSet.xml', function(err, template) {
+  if(err) {
+    console.dir(err);
+  } else {
+
+    console.log(_.template(template, params));
+  }
+});
