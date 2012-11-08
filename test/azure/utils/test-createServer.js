f3ee5c51c9587e54fce1d1de130fe966cb84dcfa
Dale Stammen <stammen@gmail.com>
2012-11-08 14:43:35 -0800

    fixed wrong url for deleting Hosted Service

--- test/azure/utils/test-createServer.js
+++ test/azure/utils/test-createServer.js
@@ -6,7 +6,7 @@ var azureApi = require('../../../lib/pkgcloud/azure2/utils/azureApi');
 var client = helpers.createClient('azure2', 'compute');
 
 var options = {
-  name: 'pkgcloud18',
+  name: 'pkgcloud6',
   flavor: 'Small',
   image: 'OpenLogic__OpenLogic-CentOS-62-20120531-en-us-30GB.vhd'
 };
