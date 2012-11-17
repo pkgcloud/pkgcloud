4e20e1514e6f8cc28f30d2221c965615f47ad381
Dale Stammen <stammen@gmail.com>
2012-11-17 11:22:19 -0800

    fixed test driver for win32. needed better path to vows.cmd in node_modules/.bin

--- test/test.js
+++ test/test.js
@@ -1,9 +1,3 @@
-var spawn = require('child_process').spawn,
-  util = require('util'),
-  vows;
-
-process.env.NOCK = 'on';
-
 // spawns the following vows command line in a cross platform manner
 // NOCK=on vows test/*/*/*-test.js --spec --isolate"
 
@@ -11,15 +5,26 @@ process.env.NOCK = 'on';
 // "echo y  | ssh-keygen -f test/fixtures/testkey -q -t rsa -N '' 2>&1 > /dev/null && is no longer
 // needed in the test command line. testkey are checked into tests/fixtures/testkey and testkey.pub
 
+var path = require('path'),
+  spawn = require('child_process').spawn,
+  args = ['-i', '--spec','--color', './test/*/*/*-test.js'],
+  options = {cwd : process.cwd()},
+  vowsPath = (process.platform === 'win32') ? path.resolve('./node_modules/.bin/vows.cmd') : 'vows',
+  util = require('util'),
+  vows;
 
-var args = ['./node_modules/.bin/vows', '-i', '--spec','--color','./test/*/*/*-test.js'];
+// turn on NOCK
+process.env.NOCK = 'on';
 
-vows = spawn(process.execPath, args);
+// run vows testing
+vows = spawn(vowsPath, args, options);
 
+// test results
 vows.stdout.on('data', function (data) {
   util.print(data);
 });
 
+// test errors
 vows.stderr.on('data', function (data) {
   util.error(data);
 });
