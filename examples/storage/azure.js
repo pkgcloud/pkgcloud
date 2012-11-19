f9d62602dcc653edc7279d27161e0d2c5ee953bf
indexzero <charlie.robbins@gmail.com>
2012-11-19 09:51:25 +0900

    [doc] Final pass on examples

--- examples/storage/azure.js
+++ examples/storage/azure.js
@@ -1,1 +1,7 @@
 var pkgcloud = require('../../lib/pkgcloud');
+
+var azure = pkgcloud.storage.createClient({
+  provider: 'azure',
+  storageAccount: "test-storage-account",			  // Name of your storage account
+  storageAccountKey: "test-storage-account-key"	// Access key for storage account
+});
