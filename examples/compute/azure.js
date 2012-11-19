cf39397e683deb1c9f79d9ea801a8a3f3d410327
indexzero <charlie.robbins@gmail.com>
2012-11-19 09:51:25 +0900

    [doc] Final pass on examples

--- examples/compute/azure.js
+++ examples/compute/azure.js
@@ -1,1 +1,27 @@
 var pkgcloud = require('../../lib/pkgcloud');
+
+var azure = pkgcloud.compute.createClient({
+  provider: 'azure',
+  storageAccount: 'test-storage-account',			    // Name of your storage account
+  storageAccountKey: 'test-storage-account-key', 	// Access key for storage account
+  managementCertificate: './test/fixtures/azure/cert/management/management.pem',
+  subscriptionId: "azure-account-subscription-id",
+  azure: {
+    location: 'East US',	  // Azure location for server
+    username: 'pkgcloud',	  // Username for server
+    password: 'Pkgcloud!!',	// Password for server
+    //
+    // SSH settings for linux server
+    //
+    ssh: {					        
+      port: 22,			        // default is 22
+      pem: "./test/fixtures/azure/cert/ssh/mycert.pem",
+      pemPassword: ""
+    },
+    //
+    // RDP settings for windows server
+    //
+    rdp: {	
+      port: 3389
+    }
+});
