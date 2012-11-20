a14a266ed035773a8ef9f65163dfa3bb1b307062
Dale Stammen <stammen@gmail.com>
2012-11-19 16:08:45 -0800

    changed to vows test

--- test/azure/compute/client/test-createServer.js
+++ test/azure/compute/client/test-createServer.js
@@ -1,30 +1,97 @@
-//TODO: Make this a vows test
+/*
+ * create-server-test.js: Azure specific create server test
+ *
+ * (C) 2012 MSOpenTech Inc.
+ *
+ */
 
-var Client = new require('../../../../lib/pkgcloud/core/base/client').Client;
-var helpers = require('../../../helpers');
-var pkgcloud = require('../../../../lib/pkgcloud');
+
+var fs = require('fs'),
+  path = require('path'),
+  vows = require('vows'),
+  assert = require('../../../helpers/assert'),
+  helpers = require('../../../helpers'),
+  azureNock = require('../../../helpers/azureNock'),
+  nock   = require('nock');
+
+var options = {
+  name: 'create-test-ids2',
+  flavor: 'ExtraSmall',
+  image: 'CANONICAL__Canonical-Ubuntu-12-04-amd64-server-20120528.1.3-en-us-30GB.vhd'
+};
+
+function testCreateServer (client) {
+  var name   = 'azure',
+    test   = {};
+
+  test["The pkgcloud " + name + " compute client"] = {
+    "the createServer() method": {
+      "with image and flavor ids": {
+        topic: function () {
+          client.createServer({
+            name: options.name,
+            image: options.image,
+            flavor: options.flavor
+          }, this.callback);
+        },
+        "should return a valid server": function (err, server) {
+
+          testContext.server = server;
+          assert.isNull(err);
+          if(err === null) {
+            assert.equal(server.name, options.name);
+            assert.equal(server.imageId, options.image);
+            assert.assertServerDetails(server);
+          }
+        }
+      }
+    }
+  };
+
+  return test;
+}
+
+function testSetWait (client) {
+  var name   = 'azure',
+    test   = {};
+
+  test["The pkgcloud " + name + " compute client"] = {
+    "the setWait() method": {
+      "with setWait({ status: 'RUNNING' },": {
+        topic: function () {
+          testContext.server.setWait({ status: 'RUNNING' }, 1000, this.callback);
+        },
+        "should return a running server": function (err, server) {
+          testContext.server = server;
+          assert.isNull(err);
+          if(err === null) {
+            assert.equal(server.name, options.name);
+            assert.equal(server.status, 'RUNNING');
+            assert.equal(server.imageId, options.image);
+            assert.assertServerDetails(server);
+          }
+        }
+      }
+    }
+  };
+
+  return test;
+}
 
 var client = helpers.createClient('azure', 'compute');
 
-var options = {
-  name: 'pkgcloud3',
-  flavor: 'Small',
-  image: 'OpenLogic__OpenLogic-CentOS-62-20120531-en-us-30GB.vhd'
-};
+var testContext = {};
 
-client.createServer(options, function(err, server) {
-  if(err) {
-    console.log(err);
-  } else {
-    server.setWait({ status: 'RUNNING' }, 5000, function (err, srv) {
-       if(err) {
-         console.log(err);
-       } else {
-         console.log(srv);
-       }
-     });
-  }
-});
+if (process.env.NOCK) {
+  azureNock.serverTest(nock, helpers);
+}
+
+vows
+  .describe('pkgcloud/azure/compute/createServer')
+  .addBatch(testCreateServer(client))
+  .addBatch(testSetWait(client))
+  ["export"](module);
+
 
 
 
