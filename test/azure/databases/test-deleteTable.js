0f0c5ff965d3b58deef70374e45e43591ada09bd
Dale Stammen <stammen@gmail.com>
2012-12-10 17:51:30 -0800

    changed delete to remove

--- test/azure/databases/test-deleteTable.js
+++ test/azure/databases/test-deleteTable.js
@@ -5,8 +5,7 @@ var helpers = require('../../helpers');
 
 var client = helpers.createClient('azure', 'database');
 
-
-client.delete('test2', function(err, res) {
+client.remove('test2', function(err, res) {
   if(err) {
     console.dir(err);
   } else {
