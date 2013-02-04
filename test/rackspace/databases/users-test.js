1f79b56fa5a737926eba7d458f656d244994a03c
Jarrett Cruger <jcruger@qrhook.com>
2013-02-04 17:43:50 -0500

    Added test for adding user with invalid characters

--- test/rackspace/databases/users-test.js
+++ test/rackspace/databases/users-test.js
@@ -47,6 +47,10 @@ if (process.env.NOCK) {
       "{\"users\":[{\"name\":\"joeTestFour\",\"password\":\"joepasswd\",\"databases\":[]},{\"name\":\"joeTestFive\",\"password\":\"joepasswd\",\"databases\":[]}]}")
       .reply(202)
 
+    .post('/v1.0/537645/instances/51a28a3e-2b7b-4b5a-a1ba-99b871af2c8f/users',
+      "{\"users\":[{\"name\":\"@joeTestSix\",\"password\":\"joepasswd\",\"databases\":[]}]}")
+      .reply(202)
+
     .get('/v1.0/537645/instances?')
       .reply(200, helpers.loadFixture('rackspace/databaseInstances.json'))
 
@@ -95,6 +99,8 @@ if (process.env.NOCK) {
     .get('/v1.0/537645/instances?')
       .reply(200, helpers.loadFixture('rackspace/databaseInstances.json'))
 
+    .get('/v1.0/537645/instances?')
+      .reply(200, helpers.loadFixture('rackspace/databaseInstances.json'))
     .get('/v1.0/537645/instances?')
       .reply(200, helpers.loadFixture('rackspace/databaseInstances.json'))
 }
@@ -165,6 +171,22 @@ vows.describe('pkgcloud/rackspace/databases/users').addBatch({
         assert.isNull(err);
         assert.ok(response);
         assert.equal(response.statusCode, 202);
+      }
+    },
+    "create user with questionable characters": {
+      topic: function () {
+        var self = this;
+        helpers.selectInstance(client, function (instance) {
+          client.createUser({
+            username: '@joeTestSix',
+            password: 'joepasswd',
+            database: 'TestDatabase',
+            instance: instance
+          }, self.callback)
+        });
+      },
+      "should respond with error": function (err, response) {
+        assert.assertError(err);
       }
     }
   }
