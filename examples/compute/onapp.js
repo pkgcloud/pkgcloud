82e816f6afb400ef3dbff7b4e74152b184fd0377
Pedro Dias <petermdias@gmail.com>
2013-02-26 04:53:18 +0000

    onapp clouds support v1.0

--- examples/compute/onapp.js
+++ examples/compute/onapp.js
@@ -1,9 +1,36 @@
 var pkgcloud = require('../../lib/pkgcloud');
 
 
-var otherClient = pkgcloud.compute.createClient({
+var client = pkgcloud.compute.createClient({
   provider: 'onapp',
-  username: 'your-account', 
-  password: 'your-password',
-  serversUrl: 'https://'
+  username: 'your_email', 
+  password: 'api_key',
+  serversUrl: 'http://127.0.0.1'
+});
+
+//getServer
+client.getServer("gahi7dcway9qb0", function (err, server) {
+  console.log(server);
+});
+
+//setVersion
+client.getVersion(function (err, version) {
+  console.log(version);
+});
+
+//createServer
+var options = {
+  hostname: 'tests.tests.com',
+  template_id: '6',
+};
+
+client.createServer(options, function (err, server) {
+  console.log(server);
+});
+
+//getServers
+client.getServers(function (err, servers) {
+  servers[0].destroy(function(err, server){
+    console.log(server);
+  });
 });
