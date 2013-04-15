568fa22affa187024eefad059be409cf22c61a9d
Ken Perkins <ken.perkins@rackspace.com>
2013-04-15 06:51:31 -0700

    Adding more identity tests

--- test/openstack/identity/identity-test.js
+++ test/openstack/identity/identity-test.js
@@ -66,7 +66,7 @@ vows.describe('createIdentity').addBatch({
     topic: function () {
       identity.createIdentity({
         url: 'http://my.authendpoint.com',
-        username: 'demo'
+        username: 'MOCK-USERNAME'
       }, this.callback);
     },
     'should callback with an error': function (err, _) {
@@ -93,7 +93,7 @@ vows.describe('createIdentity').addBatch({
         .post('/v2.0/tokens', {
           auth: {
             passwordCredentials: {
-              username: 'demo',
+              username: 'MOCK-USERNAME',
               password: 'asdf1234'
             }
           }
@@ -104,7 +104,7 @@ vows.describe('createIdentity').addBatch({
         .post('/v2.0/tokens', {
           auth: {
             passwordCredentials: {
-              username: 'demo',
+              username: 'MOCK-USERNAME',
               password: 'asdf1234'
             },
             tenantId: '72e90ecb69c44d0296072ea39e537041'
@@ -114,7 +114,7 @@ vows.describe('createIdentity').addBatch({
 
       identity.createIdentity({
         url: 'http://my.authendpoint.com',
-        username: 'demo',
+        username: 'MOCK-USERNAME',
         password: 'asdf1234',
         region: 'Calxeda-AUS1'
       }, this.callback);
@@ -122,6 +122,138 @@ vows.describe('createIdentity').addBatch({
     'should return an identity': function (err, id) {
       assert.isNull(err);
       assert.instanceOf(id, identity.Identity);
+    }
+  },
+  'with incorrect region': {
+    topic: function () {
+
+      nock('http://my.authendpoint.com')
+        .post('/v2.0/tokens', {
+          auth: {
+            passwordCredentials: {
+              username: 'MOCK-USERNAME',
+              password: 'asdf1234'
+            }
+          }
+        })
+        .replyWithFile(200, __dirname + '/../../fixtures/openstack/initialToken.json')
+        .get('/v2.0/tenants')
+        .replyWithFile(200, __dirname + '/../../fixtures/openstack/tenantId.json')
+        .post('/v2.0/tokens', {
+          auth: {
+            passwordCredentials: {
+              username: 'MOCK-USERNAME',
+              password: 'asdf1234'
+            },
+            tenantId: '72e90ecb69c44d0296072ea39e537041'
+          }
+        })
+        .replyWithFile(200, __dirname + '/../../fixtures/openstack/realToken.json');
+
+      identity.createIdentity({
+        url: 'http://my.authendpoint.com',
+        username: 'MOCK-USERNAME',
+        password: 'asdf1234',
+        region: 'foo'
+      }, this.callback);
+    },
+    'should fail to match region in catalog': function (err, id) {
+      assert.isUndefined(id);
+      assert.instanceOf(err, Error);
+      assert.equal(err.message, 'Unable to identify target endpoint for Service: volume');
+    }
+  },
+  'with no region and regionless service catalog': {
+    topic: function () {
+
+      nock('http://my.authendpoint.com')
+        .post('/v2.0/tokens', {
+          auth: {
+            passwordCredentials: {
+              username: 'MOCK-USERNAME',
+              password: 'asdf1234'
+            }
+          }
+        })
+        .replyWithFile(200, __dirname + '/../../fixtures/openstack/initialToken.json')
+        .get('/v2.0/tenants')
+        .replyWithFile(200, __dirname + '/../../fixtures/openstack/tenantId.json')
+        .post('/v2.0/tokens', {
+          auth: {
+            passwordCredentials: {
+              username: 'MOCK-USERNAME',
+              password: 'asdf1234'
+            },
+            tenantId: '72e90ecb69c44d0296072ea39e537041'
+          }
+        })
+        .replyWithFile(200, __dirname + '/../../fixtures/openstack/realToken-noRegion.json');
+
+      identity.createIdentity({
+        url: 'http://my.authendpoint.com',
+        username: 'MOCK-USERNAME',
+        password: 'asdf1234'
+      }, this.callback);
+    },
+    'should return an identity': function (err, id) {
+      assert.isNull(err);
+      assert.instanceOf(id, identity.Identity);
+    }
+  },
+  'with no tenants listed from /v2.0/tenants': {
+    topic: function () {
+
+      nock('http://my.authendpoint.com')
+        .post('/v2.0/tokens', {
+          auth: {
+            passwordCredentials: {
+              username: 'MOCK-USERNAME',
+              password: 'asdf1234'
+            }
+          }
+        })
+        .replyWithFile(200, __dirname + '/../../fixtures/openstack/initialToken.json')
+        .get('/v2.0/tenants')
+        .replyWithFile(200, __dirname + '/../../fixtures/openstack/no-tenants.json');
+
+      identity.createIdentity({
+        url: 'http://my.authendpoint.com',
+        username: 'MOCK-USERNAME',
+        password: 'asdf1234'
+      }, this.callback);
+    },
+    'should return an error': function (err, _) {
+      assert.isUndefined(_);
+      assert.instanceOf(err, Error);
+      assert.equal(err.message, 'Unable to find tenants');
+    }
+  },
+  'with no active tenants listed from /v2.0/tenants': {
+    topic: function () {
+
+      nock('http://my.authendpoint.com')
+        .post('/v2.0/tokens', {
+          auth: {
+            passwordCredentials: {
+              username: 'MOCK-USERNAME',
+              password: 'asdf1234'
+            }
+          }
+        })
+        .replyWithFile(200, __dirname + '/../../fixtures/openstack/initialToken.json')
+        .get('/v2.0/tenants')
+        .replyWithFile(200, __dirname + '/../../fixtures/openstack/no-activeTenants.json');
+
+      identity.createIdentity({
+        url: 'http://my.authendpoint.com',
+        username: 'MOCK-USERNAME',
+        password: 'asdf1234'
+      }, this.callback);
+    },
+    'should return an error': function (err, _) {
+      assert.isUndefined(_);
+      assert.instanceOf(err, Error);
+      assert.equal(err.message, 'Unable to find an active tenant');
     }
   }
 }).export(module);
