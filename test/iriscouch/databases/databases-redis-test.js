c9699c76a9dd229b34fd059c302b045e01bb87e1
Ken Perkins <ken.perkins@rackspace.com>
2013-04-30 10:24:28 -0700

    Adding support for IrisCouch tests

--- test/iriscouch/databases/databases-redis-test.js
+++ test/iriscouch/databases/databases-redis-test.js
@@ -1,93 +1,135 @@
-///*
-// * databases-redis-test.js: Tests for IrisCouch Redis database service
-// *
-// * (C) 2012 Nodejitsu Inc.
-// * MIT LICENSE
-// *
-// */
+/*
+* databases-redis-test.js: Tests for IrisCouch Redis database service
+*
+* (C) 2012 Nodejitsu Inc.
+* MIT LICENSE
+*
+*/
+
+var helpers = require('../../helpers'),
+    should  = require('should'),
+    hock    = require('hock'),
+    mock    = !!process.env.NOCK;
+
+describe('pkgcloud/iriscouch/databases-redis', function () {
+  var context = {}, client, server;
+
+  before(function (done) {
+    client = helpers.createClient('iriscouch', 'database');
+
+    if (!mock) {
+      return done();
+    }
+
+    hock.createHock(12345, function (err, hockClient) {
+      should.not.exist(err);
+      should.exist(hockClient);
+
+      server = hockClient;
+      done();
+    });
+  });
+
+  it('the create() method with correct options should respond correctly', function(done) {
+    var subdomain = (mock ? 'nodejitsudb43639' : 'nodejitsudb' + Math.floor(Math.random() * 100000));
+    context.tempPassword = (mock ? 'sTTi:lh9vCF[' : randomPassword(12).replace("\\", ""));
+    
+    if (mock) {
+      server
+        .post('/hosting_public', helpers.loadFixture('iriscouch/database-redis.json'))
+        .reply(201, { ok: true,
+          id: 'Redis/nodejitsudb43639',
+          rev: '1-63cf360ebc115cdc8a709a910fdef6d7'
+        });
+    }
+    
+    client.create({
+      subdomain: subdomain,
+      first_name: "Marak",
+      last_name: "Squires",
+      email: "marak.squires@gmail.com",
+      type: "redis", // For redis instead of couch just put type to redis
+      password: context.tempPassword
+    }, function(err, database) {
+      should.not.exist(err);
+      should.exist(database);
+      should.exist(database.id);
+      should.exist(database.uri);
+      context.databaseId = database.id;
+      database.password.should.equal([database.host, context.tempPassword].join(':'));
+
+      server && server.done();
+      done();
+    });
+  });
+
+  describe('the create() method with invalid options like', function() {
+    it('no options should respond with errors', function(done) {
+      client.create(function(err, database) {
+        should.exist(err);
+        should.not.exist(database);
+        done();
+      });
+    });
+
+    it('no invalid options should respond with errors', function (done) {
+      client.create({ invalid: 'keys' }, function (err, database) {
+        should.exist(err);
+        should.not.exist(database);
+        done();
+      });
+    });
+
+    it('no email should respond with errors', function (done) {
+      client.create({
+          subdomain: 'testDatabase',
+          first_name: 'Daniel',
+          last_name: 'Aristizabal'},
+        function (err, database) {
+          should.exist(err);
+          should.not.exist(database);
+          done();
+        });
+    });
+
+    it('no subdomain should respond with errors', function (done) {
+      client.create({
+          email: 'daniel@nodejitsu.com',
+          first_name: 'Daniel',
+          last_name: 'Aristizabal'},
+        function (err, database) {
+          should.exist(err);
+          should.not.exist(database);
+          done();
+        });
+    });
+
+    it('no names should respond with errors', function (done) {
+      client.create({ email: 'daniel@nodejitsu.com', subdomain: 'testDatabase'},
+        function (err, database) {
+          should.exist(err);
+          should.not.exist(database);
+          done();
+        });
+    });
+  });
+
+  after(function (done) {
+    if (!mock) {
+      return done();
+    }
+
+    server.close(done);
+  });
+
+});
+
 //
-//var vows    = require('vows'),
-//    helpers = require('../../helpers'),
-//    assert  = require('../../helpers/assert'),
-//    nock    = require('nock');
+// Just a quick and lazy random password generator
 //
-//var client = helpers.createClient('iriscouch', 'database'),
-//    testContext = {};
-//
-//if (process.env.NOCK) {
-//  nock('https://hosting.iriscouch.com:443')
-//    .post('/hosting_public', helpers.loadFixture('iriscouch/database-redis.json'))
-//  .reply(201,
-//    "{\"ok\":true,\"id\":\"Redis/nodejitsudb43639\",\"rev\":\"1-63cf360ebc115cdc8a709a910fdef6d7\"}\n");
-//}
-//
-//vows.describe('pkgcloud/iriscouch/databases-redis').addBatch({
-//  "The pkgcloud IrisCouch client": {
-//    "the create() method": {
-//      "with correct options": {
-//        topic: function () {
-//          var subdomain = ((process.env.NOCK) ? 'nodejitsudb43639' : 'nodejitsudb' + Math.floor(Math.random()*100000));
-//          testContext.tempPassword = ((process.env.NOCK) ? 'sTTi:lh9vCF[' : randomPassword(12).replace("\\", ""));
-//          client.create({
-//            subdomain: subdomain,
-//            first_name: "Marak",
-//            last_name: "Squires",
-//            email: "marak.squires@gmail.com",
-//            // For redis instead of couch just put type to redis
-//            type: "redis",
-//            password: testContext.tempPassword
-//          }, this.callback);
-//        },
-//        "should respond correctly": function (err, database) {
-//          assert.isNull(err);
-//          assert.ok(database.id);
-//          assert.ok(database.uri);
-//          testContext.databaseId = database.id;
-//          assert.equal(database.password, [database.host, testContext.tempPassword].join(':'));
-//        }
-//      },
-//      "with invalid options like": {
-//        "no options": {
-//          topic: function () {
-//            client.create(this.callback);
-//          },
-//          "should respond with errors": assert.assertError
-//        },
-//        "invalid options": {
-//          topic: function () {
-//            client.create({ invalid:'keys' }, this.callback);
-//          },
-//          "should respond with errors": assert.assertError,
-//        },
-//        "no email": {
-//          topic: function () {
-//            client.create({ subdomain:'testDatabase', first_name: "Daniel", last_name: "Aristizabal"}, this.callback);
-//          },
-//          "should respond with errors": assert.assertError
-//        },
-//        "no subdomain": {
-//          topic: function () {
-//            client.create({ email: "daniel@nodejitsu.com", first_name: "Daniel", last_name: "Aristizabal"}, this.callback);
-//          },
-//          "should respond with errors": assert.assertError
-//        },
-//        "no names": {
-//          topic: function () {
-//            client.create({ email: "daniel@nodejitsu.com", subdomain:'testDatabase'}, this.callback);
-//          },
-//          "should respond with errors": assert.assertError
-//        }
-//      }
-//    }
-//  }
-//}).export(module);
-//
-////
-//// Just a quick and lazy random password generator
-////
-//function randomPassword(length) {
-//  if (length == 1) {
-//    return String.fromCharCode(Math.floor(Math.random() * (122 - 48 + 1)) + 48);
-//  }
-//  return String.fromCharCode(Math.floor(Math.random() * (122 - 48 + 1)) + 48) + randomPassword(length - 1);
-//}
+function randomPassword(length) {
+  if (length == 1) {
+    return String.fromCharCode(Math.floor(Math.random() * (122 - 48 + 1)) + 48);
+  }
+  return String.fromCharCode(Math.floor(Math.random() * (122 - 48 + 1)) + 48) + randomPassword(length - 1);
+}
--- test/iriscouch/databases/databases-test.js
+++ test/iriscouch/databases/databases-test.js
@@ -1,86 +1,122 @@
-///*
-// * databases-test.js: Tests for IrisCouch databases service
-// *
-// * (C) 2012 Nodejitsu Inc.
-// * MIT LICENSE
-// *
-// */
-//
-//var vows    = require('vows'),
-//    helpers = require('../../helpers'),
-//    assert  = require('../../helpers/assert'),
-//    nock    = require('nock');
-//
-//var client = helpers.createClient('iriscouch', 'database'),
-//    testContext = {};
-//
-//if (process.env.NOCK) {
-//  nock('https://hosting.iriscouch.com:443')
-//    .post('/hosting_public', helpers.loadFixture('iriscouch/database.json'))
-//      .reply(201,
-//        "{\"ok\":true,\"id\":\"Server/nodejitsudb908\",\"rev\":\"1-dc91e4ee524420e6f32607b0c24151de\"}\n");
-//
-//  nock('http://nodejitsudb908.iriscouch.com')
-//    .get('/')
-//      .reply(404, "Host not found: nodejitsudb908.iriscouch.com")
-//    .get('/')
-//      .reply(404, "Host not found: nodejitsudb908.iriscouch.com")
-//    .get('/')
-//      .reply(200, "{\"couchdb\":\"Welcome\",\"version\":\"1.2.0\"}\n");
-//}
-//
-//vows.describe('pkgcloud/iriscouch/databases').addBatch({
-//  "The pkgcloud IrisCouch client": {
-//    "the create() method": {
-//      "with correct options": {
-//        topic: function () {
-//          var subdomain = ((process.env.NOCK) ? 'nodejitsudb908' : 'nodejitsudb' + Math.floor(Math.random()*100000));
-//          client.create({
-//            subdomain: subdomain,
-//            first_name: "Marak",
-//            last_name: "Squires",
-//            email: "marak.squires@gmail.com"
-//          }, this.callback);
-//        },
-//        "should respond correctly": function (err, database) {
-//          assert.isNull(err);
-//          assert.ok(database.id);
-//          assert.ok(database.uri);
-//          testContext.databaseId = database.id;
-//        }
-//      },
-//      "with invalid options like": {
-//        "no options": {
-//          topic: function () {
-//            client.create(this.callback);
-//          },
-//          "should respond with errors": assert.assertError
-//        },
-//        "invalid options": {
-//          topic: function () {
-//            client.create({ invalid:'keys' }, this.callback);
-//          },
-//          "should respond with errors": assert.assertError,
-//        },
-//        "no email": {
-//          topic: function () {
-//            client.create({ subdomain:'testDatabase', first_name: "Daniel", last_name: "Aristizabal"}, this.callback);
-//          },
-//          "should respond with errors": assert.assertError
-//        },
-//        "no subdomain": {
-//          topic: function () {
-//            client.create({ email: "daniel@nodejitsu.com", first_name: "Daniel", last_name: "Aristizabal"}, this.callback);
-//          },
-//          "should respond with errors": assert.assertError
-//        },
-//        "no names": {
-//          topic: function () {
-//            client.create({ email: "daniel@nodejitsu.com", subdomain:'testDatabase'}, this.callback);
-//          },
-//          "should respond with errors": assert.assertError
-//        }
-//      }
-//    }
-//  }
-//}).export(module);
+/*
+ * databases-test.js: Tests for IrisCouch database service
+ *
+ * (C) 2012 Nodejitsu Inc.
+ * MIT LICENSE
+ *
+ */
+
+var helpers = require('../../helpers'),
+    should = require('should'),
+    hock = require('hock'),
+    mock = !!process.env.NOCK;
+
+describe('pkgcloud/iriscouch/databases', function () {
+  var context = {}, client, server;
+
+  before(function (done) {
+    client = helpers.createClient('iriscouch', 'database');
+
+    if (!mock) {
+      return done();
+    }
+
+    hock.createHock(12345, function (err, hockClient) {
+      should.not.exist(err);
+      should.exist(hockClient);
+
+      server = hockClient;
+      done();
+    });
+  });
+
+  it('the create() method with correct options should respond correctly', function (done) {
+    var subdomain = (mock ? 'nodejitsudb908' : 'nodejitsudb' + Math.floor(Math.random() * 100000));
+
+    if (mock) {
+      server
+        .post('/hosting_public', helpers.loadFixture('iriscouch/database.json'))
+        .reply(201, {
+          ok: true,
+          id: 'Server/nodejitsudb908',
+          rev: '1-dc91e4ee524420e6f32607b0c24151de'
+        });
+    }
+
+    client.create({
+      subdomain: subdomain,
+      first_name: 'Marak',
+      last_name: 'Squires',
+      email: 'marak.squires@gmail.com'
+    }, function (err, database) {
+      should.not.exist(err);
+      should.exist(database);
+      should.exist(database.id);
+      should.exist(database.uri);
+      context.databaseId = database.id;
+
+      server && server.done();
+      done();
+    });
+  });
+
+  describe('the create() method with invalid options like', function () {
+    it('no options should respond with errors', function (done) {
+      client.create(function (err, database) {
+        should.exist(err);
+        should.not.exist(database);
+        done();
+      });
+    });
+
+    it('no invalid options should respond with errors', function (done) {
+      client.create({ invalid: 'keys' }, function (err, database) {
+        should.exist(err);
+        should.not.exist(database);
+        done();
+      });
+    });
+
+    it('no email should respond with errors', function (done) {
+      client.create({
+          subdomain: 'testDatabase',
+          first_name: 'Daniel',
+          last_name: 'Aristizabal'},
+        function (err, database) {
+          should.exist(err);
+          should.not.exist(database);
+          done();
+        });
+    });
+
+    it('no subdomain should respond with errors', function (done) {
+      client.create({
+          email: 'daniel@nodejitsu.com',
+          first_name: 'Daniel',
+          last_name: 'Aristizabal'},
+        function (err, database) {
+          should.exist(err);
+          should.not.exist(database);
+          done();
+        });
+    });
+
+    it('no names should respond with errors', function (done) {
+      client.create({ email: 'daniel@nodejitsu.com', subdomain: 'testDatabase'},
+        function (err, database) {
+          should.exist(err);
+          should.not.exist(database);
+          done();
+        });
+    });
+  });
+
+  after(function (done) {
+    if (!mock) {
+      return done();
+    }
+
+    server.close(done);
+  });
+
+});
