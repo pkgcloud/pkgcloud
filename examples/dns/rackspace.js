db682caffba22ce5453c0bcf599c5bcdb3d79ec5
rdodev <rubenoz@gmail.com>
2013-12-24 11:43:57 -0500

    Rebasing

--- examples/dns/rackspace.js
+++ examples/dns/rackspace.js
@@ -3,8 +3,8 @@ var _		 = require('underscore');
 
 var rackspace = pkgcloud.dns.createClient({
     provider: 'rackspace',
-    username: 'rdodev',
-    apiKey: '2706535fb997436b8e9c28db909df003'
+    username: 'rax-user-id',
+    apiKey: '1234567890asdbchehe'
   });
 
 // Basic DNS management operations. Please note that due to the asynchronous nature of JS programming,
@@ -23,7 +23,7 @@ rackspace.getZones({}, function (err, zones) {
 // 2 - Create a new "Zone". The details object has these required fields: name, admin email, 
 // ttl and comment are optional. *IMPORTANT*: Currently the service will check the domain name you are 
 // trying to use is actually registered. If it cannot find a record for it, it will error out.
-var details = {name: 'clearthefog.com', email: 'admin@clearthefog.com', ttl: 300, comment: 'I pity .foo'};
+var details = {name: 'domain.name', email: 'admin@domain.name', ttl: 300, comment: 'I pity .foo'};
 rackspace.createZone(details, function (err, zone) {
 	if(!err) {
 		console.log(zone.id + ' ' + zone.name + ' ' + zone.ttl);
@@ -31,7 +31,7 @@ rackspace.createZone(details, function (err, zone) {
 });
 
 // 3 - Let's get the "Zone" we just created and let's see its records
-rackspace.getZones({name:'clearthefog.com'}, function (err, zones) {
+rackspace.getZones({name:'domain.name'}, function (err, zones) {
 	if(!err && zones.length > 0) {
 		console.log('We have parent Zone');
 		rackspace.getRecords(zones[0], function (err, records) {
@@ -48,8 +48,8 @@ rackspace.getZones({name:'clearthefog.com'}, function (err, zones) {
 });
 
 // 4 - Let's add a new DNS A-record to a "Zone". Record has three required fields: type, name, data
-var _rec = {name: 'sub.clearthefog.com', type: 'A', data: '127.0.0.1'};
-rackspace.getZones({name:'clearthefog.com'}, function (err, zones) {
+var _rec = {name: 'sub.domain.name', type: 'A', data: '127.0.0.1'};
+rackspace.getZones({name:'domain.name'}, function (err, zones) {
 	if(!err && zones.length > 0) {
 		console.log('We have parent Zone');
 		rackspace.createRecord(zones[0], _rec, function (err, rec) {
@@ -62,7 +62,7 @@ rackspace.getZones({name:'clearthefog.com'}, function (err, zones) {
 });
 
 // 5 - Now let's remove the "Zone" and all of its children records.
-rackspace.getZones({name:'clearthefog.com'}, function (err, zones) {
+rackspace.getZones({name:'domain.name'}, function (err, zones) {
 	if(!err && zones.length > 0) {
 		console.log('We have parent Zone');
 		rackspace.deleteZone(zones[0], function (err) {
