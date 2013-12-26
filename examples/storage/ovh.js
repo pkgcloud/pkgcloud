53c3608a075afa9924a4b954c921f9edffde458a
davide <davide@linux-dev3>
2013-12-26 15:27:02 +0100

    fixed example for git push

--- examples/storage/ovh.js
+++ examples/storage/ovh.js
@@ -2,11 +2,21 @@ var fs = require('fs'),
     pkgcloud = require('../../lib/pkgcloud'),
     _ = require('underscore');
 
+/*
+To obtain these parameters you must:
+1) have an OVH account,
+2) apply for Public Cloud Storage
+3) create an application as described in https://api.ovh.com/g934.first_step_with_api
+4) go to https://api.ovh.com and explore the API
+5) login with your account
+6) go to API command '/cloud/{serviceName}/pcs/{pcsServiceName}/credentials'
+*/
+
 var ovh = pkgcloud.storage.createClient({
   provider: 'ovh',
-  username: 'md94422-20723546',
-  password: 'uU3lnj6wX53C',
-  tenantName: 'T_md94422-20723546' 
+  username: 'username', //ex. md12345-1234567
+  password: 'password',    //ex. ksadkRjnhtr
+  tenantName: 'tenantName' // ex. T_md12345-1234567
 });
 
 // Basic container and file operations. Please note that due to the asynchronous nature of Javascript programming,
@@ -60,9 +70,7 @@ var example3= function (err, container) {
     return;
   }
 
-  //var myPicture = fs.createReadStream('/path/to/some/file/picture.jpg');
-
-  var myPicture = fs.createReadStream('/home/davide/showpic.php.png');
+  var myPicture = fs.createReadStream('/path/to/some/file/picture.jpg');
 
   myPicture.pipe(ovh.upload({
       container: container.name,
