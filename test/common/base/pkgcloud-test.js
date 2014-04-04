85ddecfb2cf8c6091302ff16eeef330c15ad5646
Kyle Kelley <kyle.kelley@rackspace.com>
2014-04-04 10:34:06 -0500

    Make synchronous

--- test/common/base/pkgcloud-test.js
+++ test/common/base/pkgcloud-test.js
@@ -2,7 +2,7 @@ var should = require('should'),
     pkgcloud = require('../../../lib/pkgcloud');
 
 describe('pkgcloud/pkgcloud', function() {
-  it('should throw an Error', function(done) {
+  it('should throw an Error', function() {
     (function () {
         pkgcloud.storage.createClient({"provider":"in-memory"});
     }).should.throw(new Error("in-memory is not a supported provider"));
