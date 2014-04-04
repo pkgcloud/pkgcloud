b51cfb1fcbb67f22bbcbf98c1f25a2549cda0fe7
Kyle Kelley <kyle.kelley@rackspace.com>
2014-04-03 19:37:10 -0500

    Test for invalid provider

---
+++ test/common/base/pkgcloud-test.js
@@ -0,0 +1,10 @@
+var should = require('should'),
+    pkgcloud = require('../../../lib/pkgcloud');
+
+describe('pkgcloud/pkgcloud', function() {
+  it('should throw an Error', function(done) {
+    (function () {
+        pkgcloud.storage.createClient({"provider":"in-memory"});
+    }).should.throw(new Error("in-memory is not a supported provider"));
+  });
+});
