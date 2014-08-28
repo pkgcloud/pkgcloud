bc2b08c4f54fc4e3034cbd1526e6be654b289439
Ken Perkins <ken.perkins@rackspace.com>
2014-08-28 09:14:52 -0700

    Cleaning up meta-test code after PR

--- test/common/compute/meta-test.js
+++ test/common/compute/meta-test.js
@@ -6,18 +6,17 @@
 */
 
 var fs = require('fs'),
-    path = require('path'),
-    qs = require('qs'),
-    should = require('should'),
-    helpers = require('../../helpers'),
-    hock = require('hock'),
-    async = require('async'),
-    _ = require('underscore'),
-    Image = require('../../../lib/pkgcloud/core/compute/image').Image,
-    mock = !!process.env.MOCK;
+  path = require('path'),
+  qs = require('qs'),
+  should = require('should'),
+  helpers = require('../../helpers'),
+  hock = require('hock'),
+  async = require('async'),
+  _ = require('underscore'),
+  Image = require('../../../lib/pkgcloud/core/compute/image').Image,
+  mock = !!process.env.MOCK;
 
-providers=["openstack"];
-
+var providers=["openstack"];
 
 providers.forEach(function (provider) {
   describe('pkgcloud/common/compute/server [' + provider + ']', function () {
@@ -66,7 +65,7 @@ providers.forEach(function (provider) {
 
         context.images = images
 
-	images.forEach(function(img) {
+        images.forEach(function(img) {
           img.should.be.instanceOf(Image);
         });
 
@@ -131,9 +130,8 @@ function setupMetaMock(client, provider, servers) {
   }
 }
 
-
 function setupImagesMock(client, provider, servers) {
-   if (provider === 'openstack') {
+  if (provider === 'openstack') {
     servers.authServer
       .post('/v2.0/tokens', {
         auth: {
@@ -160,38 +158,5 @@ function setupImagesMock(client, provider, servers) {
     servers.server
       .get('/v2/72e90ecb69c44d0296072ea39e537041/images/detail')
       .replyWithFile(200, __dirname + '/../../fixtures/openstack/images.json')
-    }
-  
-}
-
-
-/**
- * serverStatusReply()
- * fills in the nock xml reply from the server with server name and status
- * @param name - name of the server
- * @param status - status to be returned in reply
- *  status should be:
- *      ReadyRole - server is RUNNING
- *      VMStopped - server is still PROVISIONING
- *      Provisioning - server is still PROVISIONING
- *      see lib/pkgcloud/azure/compute/server.js for more status values
- *
- * @return {String} - the xml reply containing the server name and status
- */
-var serverStatusReply = function (name, status) {
-
-  var template = helpers.loadFixture('azure/server-status-template.xml'),
-    params = {NAME: name, STATUS: status};
-
-  var result = _.template(template, params);
-  return result;
-};
-
-var filterPath = function (path) {
-  var name = PATH.basename(path);
-  if (path.search('embed-detail=true') !== -1) {
-    return '/getStatus?name=' + name;
   }
-
-  return path;
-};
+}
